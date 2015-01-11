3046a344373adb7651aeac51b7003888cb8e56f3
William Short <darkhelmet125@csu.fullerton.edu>
2015-01-11 00:20:51 -0800

    Added a simple player movement script, a few materials for the player, and a few transforms. Camera doesn't follow player yet, and the capsule used for the player rotates but should not.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	public float maxSpeed;
+	private Vector3 input;
+	public GameObject startPosition;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = startPosition.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
+
+		if(rigidbody.velocity.magnitude < maxSpeed)
+			rigidbody.AddForce(input*moveSpeed);
+	}
+
+}
